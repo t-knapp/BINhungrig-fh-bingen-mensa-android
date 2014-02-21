4dd6a755559c69812a96307887412dbea876b03c
Tobias Knapp <t.knapp@fh-bingen.de>
2014-02-21 12:46:53 +0100

    Added: RatingsView. Missing Swipe.

---
+++ src/de/fhbingen/mensa/RatingsView.java
@@ -0,0 +1,22 @@
+package de.fhbingen.mensa;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class RatingsView extends View {
+
+	public RatingsView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		super.onDraw(canvas);
+		
+		//canvas.drawCircle(, cy, radius, paint)
+	}
+
+}

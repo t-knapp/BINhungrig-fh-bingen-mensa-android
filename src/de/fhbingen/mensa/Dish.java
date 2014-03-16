d766c0053fd528c7249b20055e9d998060ff9682
Tobias Knapp <t.knapp@fh-bingen.de>
2014-03-16 18:03:12 +0100

    Cleanup Dish

--- src/de/fhbingen/mensa/Dish.java
+++ src/de/fhbingen/mensa/Dish.java
@@ -108,45 +108,6 @@ public class Dish implements Serializable {
 	public int getId_pictures() {
 		return id_pictures;
 	}
-	
-
-	/** Transforms the String of Date in an valid Calendar object.
-     *  Calculates the responding day of the week with the calendar object.
-     * @return a String with the weekday e.g. Monday
-     * @throws NotExcepectedServerAnswer if the String of date doesn't match to yyyy-mm-dd
-     */
-    private String computeDayOfWeek() throws NotExcepectedServerAnswer{
-        boolean matches = Pattern.matches("\\d{4}-\\d{2}-\\d{2}", getDate());
-        assert(matches);
-        Calendar cal = Calendar.getInstance();
-        if (matches){
-           //converting String into calendar object
-            try{
-                cal.setTime(DATE_FORMAT.parse(getDate()));
-                Log.d(TAG, "Calendarvalues changed to:\n" + cal.toString());
-            }
-            catch (ParseException pe){
-                Log.e(TAG, "ParseException in Dish - computeDayOfWeek");
-                throw new NotExcepectedServerAnswer("Error while parsing");
-            }
-        }
-        else {
-            throw new NotExcepectedServerAnswer("Dateformat doesn't match to the expected result yyyy-mm-dd* ");
-        }
-        String[] DAYS = null;
-        try{
-            DAYS = context.getResources().getStringArray(R.array.dayName_array);
-        }
-        catch (NullPointerException e){
-            Log.e(TAG, "NullPointer bei days Array abrufen");
-        }
-        catch (Exception e){
-            Log.e(TAG, e.toString() +"\n" + e.getCause());
-        }
-        String retValue = DAYS[cal.get(Calendar.DAY_OF_WEEK) -1]; // Sonday = -1
-        Log.d(TAG, "returnValue of getDay " + retValue);
-        return retValue;
-    }
 
     /**
      * Method that determines if the dish is served today.
@@ -154,8 +115,6 @@ public class Dish implements Serializable {
      * @throws ParseException
      */
     public boolean isServedToday() throws ParseException {
-        Log.d(TAG, "dish.date : " + getDate());
-        Log.d(TAG, "now.date  : " + DATE_FORMAT.format(new Date()));
         return getDate().equals(DATE_FORMAT.format(new Date()));
     }
 
